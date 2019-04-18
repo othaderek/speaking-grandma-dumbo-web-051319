033a3a02fb61cc5eebe8be4e68be3d0758fc33b6
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-18 21:36:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(greeting)
-  if greeting == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-    elsif greeting == "Hi Nana, how are you?"
-    return "HUH?! SPEAK UP, SONNY!"
-    elsif greeting == 
+  if greeting == greetin
   
   
 end
